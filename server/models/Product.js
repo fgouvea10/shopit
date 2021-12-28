d89d7c2d77e6a4aa8335aec4369ad4de55e41eb1
Felipe Gouvea <felipegalvesouvea@gmail.com>
2021-12-28 00:03:55 -0300

    create product model

---
+++ server/models/Product.js
@@ -0,0 +1,92 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Product name must be provided'],
+    trim: true,
+    maxlength: [100, 'Product name cannot exceed 100 characters']
+  },
+  price: {
+    type: Number,
+    required: [true, 'Product price must be provided'],
+    maxlength: [5, 'Product price cannot exceed 5 characters']
+  },
+  description: {
+    type: String,
+    required: [true, 'Product description must be provided'],
+  },
+  ratings: {
+    type: Number,
+    default: 0,
+  },
+  images: [
+    {
+      public_id: {
+        type: String,
+        required: true
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  category: {
+    type: String,
+    required: [true, 'Product category must be provided'],
+    enum: {
+      values: [
+        'Eletronics',
+        'Cameras',
+        'Laptop',
+        'Accessories',
+        'Headphones',
+        'Food',
+        'Books',
+        'Clothes/Shoes',
+        'Beauty/Healthy',
+        'Sports',
+        'Outdoor',
+        'Home',
+      ],
+      message: 'Product category must be provided'
+    },
+  },
+  seller: {
+    type: String,
+    required: [true, 'Product seller must be provided'],
+  },
+  stock: {
+    type: Number,
+    required: [true, 'Product stock must be provided'],
+    maxlength: [5, 'Product stock cannot exceed 5 characters'],
+    default: 0,
+  },
+  numOfReviews: {
+    type: Number,
+    default: 0,
+  },
+  reviews: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: Number,
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  created_at: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Product', productSchema);
