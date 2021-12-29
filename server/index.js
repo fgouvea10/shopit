df40645c4ebb5009b7620af25dd3bf9b240b2c0e
Felipe Gouvea <felipegalvesouvea@gmail.com>
2021-12-28 21:23:09 -0300

    handling uncaught exceptions

--- server/index.js
+++ server/index.js
@@ -3,6 +3,12 @@ const connectDatabase = require("./config/database");
 
 const dotenv = require("dotenv");
 
+process.on("uncaughtException", (err) => {
+  console.log(`ERROR: ${err.message}`);
+  console.log("Shutting down the server due to Uncaught Exception.");
+  process.exit(1);
+});
+
 dotenv.config({ path: "./config/config.env" });
 
 connectDatabase();
@@ -15,7 +21,7 @@ const server = app.listen(process.env.PORT, () => {
 
 process.on("unhandledRejection", (err) => {
   console.log(`ERROR: ${err.message}`);
-  console.log("Shutting down the server due to Unhandled Promise rejection");
+  console.log("Shutting down the server due to Unhandled Promise rejection.");
   server.close(() => {
     process.exit(1);
   });
