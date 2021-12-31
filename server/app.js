c9a20dcb61ec274f966179a80baeca0cbcf8d645
Felipe Gouvea <felipegalvesouvea@gmail.com>
2021-12-31 14:02:10 -0300

    encrypting password while registration

--- server/app.js
+++ server/app.js
@@ -6,8 +6,10 @@ const errorMiddleware = require('./middlewares/errors');
 app.use(express.json());
 
 const products = require('./routes/product');
+const auth = require('./routes/auth');
 
 app.use('/api/v1', products);
+app.use('/api/v1', auth);
 
 app.use(errorMiddleware);
 
---
+++ server/controllers/AuthController.js
@@ -0,0 +1,21 @@
+const User = require("../models/User");
+const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncErrors = require("../middlewares/CatchAsyncErrors");
+
+exports.registerUser = catchAsyncErrors(async (request, response, next) => {
+  const { name, email, password } = request.body;
+  const user = await User.create({
+    name,
+    email,
+    password,
+    avatar: {
+      public_id: "avatars/kuhbasfbnkasikl",
+      url: "https://villyo.com.br/assets/img/user.png",
+    },
+  });
+
+  response.status(201).json({
+    success: true,
+    user,
+  });
+});
