249b36ea5182704a55bc4aae32d4cce195243078
Felipe Gouvea <felipegalvesouvea@gmail.com>
2022-01-02 21:44:38 -0300

    handle wrong jwt token and expire jwt error

--- server/middlewares/errors.js
+++ server/middlewares/errors.js
@@ -17,13 +17,28 @@ module.exports = (err, request, response, next) => {
 
     error.message = err.message;
 
-    if (err.name === 'CastError') {
+    if (err.name === "CastError") {
       const message = `Resource not found. Invalid ${err.path}`;
+      error = new ErrorHandler("message", 400);
+    }
+
+    if (err.name === "ValidationError") {
+      const message = Object.values(err.errors).map((value) => value.message);
       error = new ErrorHandler(message, 400);
     }
 
-    if (err.name === 'ValidationError') {
-      const message = Object.values(err.errors).map(value => value.message);
+    if (err.code === 11000) {
+      const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+      error = new ErrorHandler(message, 400);
+    }
+
+    if (err.name === "JsonWebTokenError") {
+      const message = "JSON Web Token is invalid. Try again.";
+      error = new ErrorHandler(message, 400);
+    }
+
+    if (err.name === "TokenExpiredError") {
+      const message = "JSON Web Token is expired. Try again.";
       error = new ErrorHandler(message, 400);
     }
 
