0ba62314c44b145244caee5fbf36126f9c6f7849
Felipe Gouvea <felipegalvesouvea@gmail.com>
2022-01-06 20:49:54 -0300

    add filter by price

--- web/src/App.js
+++ web/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import "./styles/global.css";
-
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 import Home from "./components/Home";
 import ProductDetails from "./components/Products/ProductDetails";
+
+import "./styles/global.css";
+import "rc-slider/assets/index.css";
 
 function App() {
   return (
--- web/src/components/Home/index.js
+++ web/src/components/Home/index.js
@@ -4,13 +4,18 @@ import { useDispatch, useSelector } from "react-redux";
 import { useAlert } from "react-alert";
 import Pagination from "react-js-pagination";
 import { useParams } from "react-router-dom";
+import Slider from "rc-slider";
 
 import { getProducts } from "../../actions/productActions";
 import Loader from "../Loader";
 import ProductCard from "../Products/ProductCard";
 
+const { createSliderWithTooltip } = Slider;
+const Range = createSliderWithTooltip(Slider.Range);
+
 function Home() {
   const [currentPage, setCurrentPage] = useState(1);
+  const [price, setPrice] = useState([1, 1000]);
 
   const dispatch = useDispatch();
   const alert = useAlert();
@@ -29,8 +34,8 @@ function Home() {
   useEffect(() => {
     if (error) return alert.error(error);
 
-    dispatch(getProducts(keyword, currentPage));
-  }, [dispatch, alert, error, keyword, currentPage]);
+    dispatch(getProducts(keyword, currentPage, price));
+  }, [dispatch, alert, error, keyword, currentPage, price]);
 
   return (
     <>
@@ -43,10 +48,43 @@ function Home() {
 
           <section id="products" className="container mt-5">
             <div className="row">
-              {products &&
-                products.map((product) => (
-                  <ProductCard key={product._id} data={product} />
-                ))}
+              {(keyword && (
+                <>
+                  <div className="col-6 col-md-3 mt-5 mb-5">
+                    <div className="px-5">
+                      <Range
+                        marks={{
+                          1: `$1`,
+                          1000: `$1000`,
+                        }}
+                        min={1}
+                        max={1000}
+                        defaultValue={[1, 1000]}
+                        tipFormatter={(value) => `$${value}`}
+                        tipProps={{
+                          placement: "top",
+                          visible: true,
+                        }}
+                        value={price}
+                        onChange={(price) => setPrice(price)}
+                      />
+                    </div>
+                  </div>
+
+                  <div className="col-6 col-md-9">
+                    <div className="row">
+                      {products &&
+                        products.map((product) => (
+                          <ProductCard key={product._id} data={product} col={4} />
+                        ))}
+                    </div>
+                  </div>
+                </>
+              )) ||
+                (products &&
+                  products.map((product) => (
+                    <ProductCard key={product._id} data={product} col={3} />
+                  )))}
             </div>
           </section>
 
