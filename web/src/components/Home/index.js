eacef24ea4e6571d93840c0c2b3631150e439667
Felipe Gouvea <felipegalvesouvea@gmail.com>
2022-01-05 22:36:25 -0300

    add pagination

--- web/src/components/Home/index.js
+++ web/src/components/Home/index.js
@@ -1,26 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Helmet } from "react-helmet";
 import { useDispatch, useSelector } from "react-redux";
 import { useAlert } from "react-alert";
+import Pagination from "react-js-pagination";
 
 import { getProducts } from "../../actions/productActions";
 import Loader from "../Loader";
 import ProductCard from "../Products/ProductCard";
 
 function Home() {
+  const [currentPage, setCurrentPage] = useState(1);
+
   const dispatch = useDispatch();
   const alert = useAlert();
 
-  const { loading, products, error, productsCount } = useSelector(
+  const { loading, products, error, productsCount, resPerPage } = useSelector(
     (state) => state.products
   );
 
+  const setCurrentPageNo = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   useEffect(() => {
     if (error) return alert.error(error);
-    
-    dispatch(getProducts());
 
-  }, [dispatch, alert, error]);
+    dispatch(getProducts(currentPage));
+  }, [dispatch, alert, error, currentPage]);
 
   return (
     <>
@@ -39,6 +45,23 @@ function Home() {
                 ))}
             </div>
           </section>
+
+          {resPerPage <= productsCount && (
+            <div className="d-flex justify-content-center mt-5">
+              <Pagination
+                activePage={currentPage}
+                itemsCountPerPage={resPerPage}
+                totalItemsCount={productsCount}
+                onChange={setCurrentPageNo}
+                nextPageText={"Next"}
+                prevPageText={"Previous"}
+                firstPageText={"First"}
+                lastPageText={"Last"}
+                itemClass="page-item"
+                linkClass="page-link"
+              />
+            </div>
+          )}
         </>
       )}
     </>
