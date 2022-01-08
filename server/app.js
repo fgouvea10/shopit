1f7b456528bd602f44d4d9f9a1aa91a86b6c147f
Felipe Gouvea <felipegalvesouvea@gmail.com>
2022-01-08 10:23:57 -0300

    change cloudinary config and add fileupload

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
+const fileUpload = require("express-fileupload");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-const cloudinary = require("cloudinary");
 const app = express();
 
 const errorMiddleware = require("./middlewares/errors");
@@ -9,12 +9,7 @@ const errorMiddleware = require("./middlewares/errors");
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_CLOUD_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+app.use(fileUpload());
 
 const products = require("./routes/product");
 const auth = require("./routes/auth");
