42c0959e7b3230f3802af556f148e2c576f765a7
Felipe Gouvea <felipegalvesouvea@gmail.com>
2022-01-09 11:47:08 -0300

    create sign in component

--- web/src/App.js
+++ web/src/App.js
@@ -6,10 +6,11 @@ import Footer from "./components/Footer";
 
 import Home from "./components/Home";
 import ProductDetails from "./components/Products/ProductDetails";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 
 import "./styles/global.css";
 import "rc-slider/assets/index.css";
-import SignIn from "./components/SignIn";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
             <Route path="/search/:keyword" element={<Home />} />
             <Route path="/product/:id" element={<ProductDetails />} />
             <Route path="/login" element={<SignIn />} />
+            <Route path="/register" element={<SignUp />} />
           </Routes>
         </div>
         <Footer />
---
+++ web/src/components/SignUp/index.js
@@ -0,0 +1,181 @@
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAlert } from "react-alert";
+import { useDispatch, useSelector } from "react-redux";
+import { Helmet } from "react-helmet";
+
+import Loader from "../Loader";
+import { register, clearErrors } from "../../actions/userActions";
+
+function SignUp() {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [avatar, setAvatar] = useState("");
+  const [avatarPreview, setAvatarPreview] = useState(
+    "/assets/default_avatar.jpg"
+  );
+  const [showPassword, setShowPassword] = useState(false);
+
+  const { name, email, password } = user;
+
+  const alert = useAlert();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { isAuthenticated, error, loading } = useSelector(
+    (state) => state.auth
+  );
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.set("name", name);
+    formData.set("email", email);
+    formData.set("password", password);
+    formData.set("avatar", avatar);
+
+    dispatch(register(formData));
+  };
+
+  const onChange = (event) => {
+    if (event.target.name === "avatar") {
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        if (reader.readyState === 2) {
+          setAvatarPreview(reader.result);
+          setAvatar(avatarPreview);
+        }
+      };
+
+      reader.readAsDataURL(event.target.files[0]);
+    } else {
+      setUser({ ...user, [event.target.name]: event.target.value });
+    }
+  };
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/");
+    }
+
+    if (error) {
+      console.log("error:", error);
+      // alert.alert(error);
+      dispatch(clearErrors());
+    }
+  }, [dispatch, alert, isAuthenticated, error]);
+
+  return (
+    <>
+      {(loading && <Loader />) || (
+        <>
+          <Helmet>
+            <title>Sign Up - ShopIT</title>
+          </Helmet>
+
+          <div className="row wrapper">
+            <div className="col-10 col-lg-5">
+              <form
+                className="shadow-lg"
+                onSubmit={handleSubmit}
+                encType="multipart/form-data"
+              >
+                <h1 className="mb-3">Register</h1>
+
+                <div className="form-group">
+                  <label htmlFor="email_field">Name</label>
+                  <input
+                    type="name"
+                    placeholder="John Doe"
+                    id="name_field"
+                    className="form-control"
+                    name="name"
+                    value={name}
+                    onChange={onChange}
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="email_field">Email</label>
+                  <input
+                    type="email"
+                    placeholder="john@example.com"
+                    id="email_field"
+                    className="form-control"
+                    name="email"
+                    value={email}
+                    onChange={onChange}
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="password_field">Password</label>
+                  <input
+                    type={(showPassword && "text") || "password"}
+                    id="password_field"
+                    placeholder="your password"
+                    className="form-control"
+                    name="password"
+                    value={password}
+                    onChange={onChange}
+                  />
+                  <button
+                    className="float-right mb-4"
+                    style={{ border: "none", background: "transparent" }}
+                    onClick={() => setShowPassword(!showPassword)}
+                  >
+                    {(showPassword && "Hide password") || "Show password"}
+                  </button>
+                </div>
+
+                <div className="form-group" style={{ paddingTop: "10px" }}>
+                  <label htmlFor="avatar_upload">Avatar</label>
+                  <div className="d-flex align-items-center">
+                    <div>
+                      <figure className="avatar mr-3 item-rtl">
+                        <img
+                          src={avatarPreview}
+                          className="rounded-circle"
+                          alt="Avatar Preview"
+                        />
+                      </figure>
+                    </div>
+                    <div className="custom-file">
+                      <input
+                        type="file"
+                        name="avatar"
+                        className="custom-file-input"
+                        id="customFile"
+                        accept="images/*"
+                        onChange={onChange}
+                      />
+                      <label className="custom-file-label" htmlFor="customFile">
+                        Choose Avatar
+                      </label>
+                    </div>
+                  </div>
+                </div>
+
+                <button
+                  id="register_button"
+                  type="submit"
+                  className="btn btn-block py-3"
+                  disabled={(loading && true) || false}
+                >
+                  REGISTER
+                </button>
+              </form>
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
+}
+
+export default SignUp;
