cf0bbcef6db3105417871d19139c9cf24d8029e9
Felipe Gouvea <felipegalvesouvea@gmail.com>
2022-01-11 00:17:42 -0300

    handle quantity on cart component

--- web/src/components/Cart/index.js
+++ web/src/components/Cart/index.js
@@ -9,6 +9,22 @@ function Cart() {
   const dispatch = useDispatch();
   const { cartItems } = useSelector((state) => state.cart);
 
+  const increaseQty = (id, quantity, stock) => {
+    const newQty = quantity + 1;
+
+    if (newQty > stock) return;
+
+    dispatch(addItemToCart(id, newQty));
+  };
+
+  const decreaseQty = (id, quantity) => {
+    const newQty = quantity - 1;
+
+    if (newQty <= 1) return;
+
+    dispatch(addItemToCart(id, newQty));
+  };
+
   return (
     <>
       <Helmet>
@@ -29,50 +45,70 @@ function Cart() {
 
           <div className="row d-flex justify-content-between">
             <div className="col-12 col-lg-8">
-              {cartItems.map(item => (
+              {cartItems.map((item) => (
                 <>
                   <hr />
-              <div className="cart-item">
-                <div className="row">
-                  <div className="col-4 col-lg-3">
-                    <img
-                      src={item.image}
-                      alt={item.name}
-                      height="90"
-                      width="115"
-                    />
-                  </div>
+                  <div className="cart-item">
+                    <div className="row">
+                      <div className="col-4 col-lg-3">
+                        <img
+                          src={item.image}
+                          alt={item.name}
+                          height="90"
+                          width="115"
+                        />
+                      </div>
 
-                  <div className="col-5 col-lg-3">
-                    <Link to={`/products/${item.product}`}>{item.name}</Link>
-                  </div>
+                      <div className="col-5 col-lg-3">
+                        <Link to={`/products/${item.product}`}>
+                          {item.name}
+                        </Link>
+                      </div>
 
-                  <div className="col-4 col-lg-2 mt-4 mt-lg-0">
-                    <p id="card_item_price">${item.price}</p>
-                  </div>
+                      <div className="col-4 col-lg-2 mt-4 mt-lg-0">
+                        <p id="card_item_price">${item.price}</p>
+                      </div>
 
-                  <div className="col-4 col-lg-3 mt-4 mt-lg-0">
-                    <div className="stockCounter d-inline">
-                      <span className="btn btn-danger minus">-</span>
-                      <input
-                        type="number"
-                        className="form-control count d-inline"
-                        value="1"
-                        readOnly
-                      />
+                      <div className="col-4 col-lg-3 mt-4 mt-lg-0">
+                        <div className="stockCounter d-inline">
+                          <span
+                            className="btn btn-danger minus"
+                            onClick={() =>
+                              decreaseQty(item.product, item.quantity)
+                            }
+                          >
+                            -
+                          </span>
+                          <input
+                            type="number"
+                            className="form-control count d-inline"
+                            value={item.quantity}
+                            readOnly
+                          />
 
-                      <span className="btn btn-primary plus">+</span>
+                          <span
+                            className="btn btn-primary plus"
+                            onClick={() =>
+                              increaseQty(
+                                item.product,
+                                item.quantity,
+                                item.stock
+                              )
+                            }
+                          >
+                            +
+                          </span>
+                        </div>
+                      </div>
+
+                      <div className="col-4 col-lg-1 mt-4 mt-lg-0">
+                        <i
+                          id="delete_cart_item"
+                          className="fa fa-trash btn btn-danger"
+                        ></i>
+                      </div>
                     </div>
                   </div>
-
-                  <div className="col-4 col-lg-1 mt-4 mt-lg-0">
-                    <i
-                      id="delete_cart_item"
-                      className="fa fa-trash btn btn-danger"
-                    ></i>
-                  </div>
-                </div>
-              </div>
                 </>
               ))}
               <hr />
