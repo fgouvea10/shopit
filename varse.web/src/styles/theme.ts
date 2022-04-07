5ec6708554add1e4a219139a3eb62e448b0b7b61
Felipe Gouvea <felipegalvesouvea@gmail.com>
2022-04-07 18:53:15 -0300

    feature: add styled components and design system

--- varse.web/src/styles/theme.ts
+++ varse.web/src/styles/theme.ts
@@ -1,35 +1,69 @@
-import { extendTheme } from '@chakra-ui/react';
-
-export const theme = extendTheme({
+export default {
   colors: {
-    gray: {
-      '900': '#181B23',
-      '800': '#1F2029',
-      '700': '#353646',
-      '600': '#4B4D63',
-      '500': '#616480',
-      '400': '#797D9A',
-      '300': '#9699B0',
-      '200': '#B3B5C6',
-      '100': '#D1D2DC',
-      '50': '#f9f9f9',
-    },
-    blue: {
-      '900': '#0E0E37',
-      '500': '#6661FE',
-      '300': '#A8A2FF',
-    },
+    primaryColorLight: '#A8A2FF',
+    primaryColorMid: '#6661FE',
+    primaryColorDark: '#0E0E37',
+
+    neutralColor100: '#F2F2FA',
+    neutralColor200: '#E1E1E7',
+    neutralColor300: '#A8A8B3',
+    neutralColor700: '#29292E',
+    neutralColor800: '#202024',
+    neutralColor900: '#121214',
+
+    semanticColorPositive: '#04D361',
+    semanticColorNegative: '#FF5343',
   },
+
   fonts: {
-    heading: 'Poppins',
-    body: 'Poppins',
+    regular: 400,
+    medium: 500,
+    semibold: 600,
+    bold: 700,
+    extrabold: 800,
   },
-  styles: {
-    global: {
-      body: {
-        bg: 'gray.50',
-        color: 'gray.800',
-      },
-    },
+
+  fontSizes: {
+    xs: '0.75rem',
+    sm: '0.875rem',
+    md: '1rem',
+    lg: '1.25rem',
+    xl: '1.5rem',
+    xxl: '2rem',
+    xxxl: '3rem',
+    xxxxl: '4rem',
   },
-});
+
+  lineHeights: {
+    shorter: '120%',
+    short: '140%',
+    tall: '160%',
+    taller: '180%',
+  },
+
+  borderRadius: {
+    base: '5px',
+    pill: '9999px',
+  },
+
+  borderWidth: {
+    thin: '1px',
+    mid: '2px',
+    thick: '4px',
+  },
+
+  spacing: {
+    spacing0: '0rem',
+    spacing1: '0.25rem',
+    spacing2: '0.5rem',
+    spacing3: '0.75rem',
+    spacing4: '1rem',
+    spacing5: '1.25rem',
+    spacing6: '1.5rem',
+    spacing8: '2rem',
+    spacing10: '2.5rem',
+    spacing12: '3rem',
+    spacing16: '4rem',
+    spacing20: '5rem',
+  },
+};
